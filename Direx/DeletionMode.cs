f3bacb31846124d160b6a6ca83cdd62841fed848
GIBSON <9ualabanana@gmail.com>
2022-08-23 21:38:17 +0300

    Improve documentation.

--- Direx/DeletionMode.cs
+++ Direx/DeletionMode.cs
@@ -3,14 +3,19 @@
 /// <summary>
 /// Extended version of the flag used in <see cref="Directory.Delete(string, bool)"/>.
 /// </summary>
-/// <remarks>
-/// <see cref="Recursive"/> and <see cref="NonRecursive"/> correspond to flag in <see cref="Directory.Delete(string, bool)"/>
-/// and <see cref="Wipe"/> is the same as <see cref="Recursive"/> but also deletes read-only files and directories
-/// instead of throwing <see cref="UnauthorizedAccessException"/> or <see cref="IOException"/>.
-/// </remarks>
 public enum DeletionMode
 {
+    /// <summary>
+    /// Same as <see cref="Recursive"/> but also deletes read-only files and directories
+    /// instead of throwing <see cref="UnauthorizedAccessException"/> or <see cref="IOException"/>.
+    /// </summary>
     Wipe,
+    /// <summary>
+    /// Correesponds to flag in <see cref="Directory.Delete(string, bool)"/> set to true.
+    /// </summary>
     Recursive,
+    /// <summary>
+    /// Correesponds to flag in <see cref="Directory.Delete(string, bool)"/> set to false.
+    /// </summary>
     NonRecursive
 }
